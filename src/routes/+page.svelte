c9d45e26dc5efc12a5395c1d7e0cf225bd67b5ac
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-08 10:59:03 +1100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Magic Catalogue</h1>
+
+<p>Things to do</p>
+<ul>
+	<li>Work out how to use SvelteKit</li>
+	<li>Create the auth system</li>
+	<li>Add saving spells and recipes</li>
+	<li>Pretty UI</li>
+	<li>Add sharing system (maybe idk)</li>
+</ul>
---
+++ src/routes/editor/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+</script>
+
+<h1>Spell editor</h1>
+
+<p>
+	<input type="text" placeholder="Name" />
+</p>
+<p><input type="summary" placeholder="Summary" /></p>
+
+<h2>Features</h2>
+<p>
+	<label for="duation">feature 1</label>
+	<input id="duration" value="value" />
+	<button>remove</button>
+</p>
+<p>
+	<button>Add new feature</button>
+</p>
+
+<h2>Components</h2>
+<p>
+	<input value="1" />
+	<input value="component 1" />
+	<button>remove</button>
+</p>
+<p>
+	<button>Add new component</button>
+</p>
+
+<h2>Procedure</h2>
+<textarea></textarea>
+
+<h2>Notes</h2>
+<textarea> </textarea>
