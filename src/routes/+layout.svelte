fc8177c3fe299339871c469cad1bc17d3cc39cf5
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-09 10:27:22 +1100

    Add fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	let { children } = $props();
+	import '../style.css';
 </script>
 
-<svelte:head></svelte:head>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Quintessential&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
 
 {@render children?.()}
