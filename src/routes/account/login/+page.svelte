04e76581852dd26f937a57d033f06273a1879fa6
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-11 10:23:56 +1100

    Improve login form appearance

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -7,15 +7,20 @@
 
 <h1>Login/Register</h1>
 <form method="post" action="?/login" use:enhance>
-  <label>
-    Username
-    <input name="username" />
-  </label>
-  <label>
-    Password
-    <input type="password" name="password" />
-  </label>
-  <button>Login</button>
-  <button formaction="?/register">Register</button>
+  <p>
+    <label>
+      Username
+      <input name="username" />
+    </label>
+  </p>
+
+  <p>
+    <label>
+      Password
+      <input type="password" name="password" />
+    </label>
+  </p>
+
+  <p><button>Login</button> <button formaction="?/register">Register</button></p>
 </form>
 <p style="color: red">{form?.message ?? ""}</p>
