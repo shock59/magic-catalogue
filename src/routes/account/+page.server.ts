1805f49182ff01408f4ffdfcb027ab6c56c4ec07
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-11 12:13:16 +1100

    Allow changing username

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,24 +1,49 @@
 import * as auth from "$lib/server/auth";
 import { fail, redirect } from "@sveltejs/kit";
 import { getRequestEvent } from "$app/server";
-import type { Actions, PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad, RequestEvent } from "./$types";
+import { db } from "$lib/server/db";
+import * as table from "$lib/server/db/schema";
+import { eq } from "drizzle-orm";
+
+let user: {
+  id: string;
+  username: string;
+};
 
 export const load: PageServerLoad = async () => {
-  const user = requireLogin();
+  user = requireLogin();
   return { user };
 };
 
 export const actions: Actions = {
-  logout: async (event) => {
-    if (!event.locals.session) {
-      return fail(401);
+  logout,
+  editUsername: async (event) => {
+    const newUsername = (await event.request.formData()).get("username");
+    if (!newUsername) {
+      return fail(400, "Please specify a new username");
     }
-    await auth.invalidateSession(event.locals.session.id);
-    auth.deleteSessionTokenCookie(event);
 
-    return redirect(302, "/account/login");
+    await db
+      .update(table.user)
+      .set({
+        username: newUsername as string,
+      })
+      .where(eq(table.user.id, user.id));
+
+    await logout(event);
   },
 };
+
+async function logout(event: RequestEvent) {
+  if (!event.locals.session) {
+    return fail(401);
+  }
+  await auth.invalidateSession(event.locals.session.id);
+  auth.deleteSessionTokenCookie(event);
+
+  return redirect(302, "/account/login");
+}
 
 function requireLogin() {
   const { locals } = getRequestEvent();
